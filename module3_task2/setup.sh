0bbab8a35a9268fe69e26c473f537dc75c9340be
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-01-12 18:23:11 +0100

    another try

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -5,4 +5,4 @@ curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_L
 sudo apt install ./hugoversion.deb
 sudo snap install golangci-lint
 sudo gem install chef-utils -v 16.6.14
-sudo gem install mdl
+sudo gem install mdl -v 0.11.0
