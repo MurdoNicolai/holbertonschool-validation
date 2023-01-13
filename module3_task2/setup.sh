4480b16303590cf85a37cab9d96e6389666fde7f
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-01-13 09:27:31 +0100

    chaing mdl to markdownlint

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -5,4 +5,5 @@ curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_L
 sudo apt install ./hugoversion.deb
 sudo snap install golangci-lint
 sudo gem install chef-utils -v 16.6.14
+sudo npm install -g markdownlint-cli
 sudo gem install mdl -v 0.11.0
