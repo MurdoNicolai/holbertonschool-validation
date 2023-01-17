8f2af8e9087737c9c4334a180196c81207f8b10c
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-01-17 14:40:42 +0100

    adding install hugo

--- module4_task1/setup.sh
+++ module4_task1/setup.sh
@@ -3,6 +3,8 @@ sudo apt-get update
 sudo apt-get install -y make golang-go git curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugoversion.deb
 sudo apt install ./hugoversion.deb
+sudo snap install hugo
+docker pull klakegg/hugo
 sudo snap install golangci-lint
 sudo gem install chef-utils -v 16.6.14
 sudo npm install -g markdownlint-cli
